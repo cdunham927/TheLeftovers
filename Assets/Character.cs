174bb0a0fbc667503f045128ca05455e1ee7f01b
asori015 <32408013+asori015@users.noreply.github.com>
2020-02-01 00:29:41 -0800

    Added health functions
    Added getters and setters for private health variables.

---
+++ Assets/Character.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField]
+    private int cur_health = 12;
+    [SerializeField]
+    private int max_health = 12;
+
+    public int getHealth() {
+        return cur_health;
+    }
+
+    public void setHealth(int x) {
+        cur_health = x;
+        return;
+    }
+
+    public int getMaxHealth() {
+        return max_health;
+    }
+
+    public void setMaxHealth(int x) {
+        max_health = x;
+        return;
+    }
+
+    public void takeDamage(int d) {
+        cur_health -= d;
+        if(cur_health <= 0) {
+            cur_health = 0;
+            //gamecontroller.hasDied();
+        }
+        return;
+    }
+
+    public void healCharacter(int h) {
+        cur_health += h;
+        if(cur_health >= max_health) {
+            cur_health = max_health;
+        }
+        return;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
